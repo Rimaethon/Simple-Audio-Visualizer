59c46fc30be096c3ead36a15036a0c093a20a8d9
Yoldaş Ulaş <yutalas@gmail.com>
2023-07-29 17:03:49 +0300

    Add AudioSyncColor script to MusicSync feature
    This commit introduces a new AudioSyncColor script to the MusicSync feature. The script allows music beats to trigger color changes on a selected Image component. It uses a coroutine to smoothly transition the Image's color to a new color (randomly selected from red, blue, and green) on each beat. This enhancement aims to increase the level of user interactivity and dynamism in the game.

---
+++ Assets/Rimaethon/_Scripts/MusicSync/AudioSyncColor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rimaethon._Scripts.MusicSync
+{
+    [RequireComponent(typeof(Image))]
+    public class AudioSyncColor : AudioSyncer 
+    {
+        private Image imageComponent;
+        private Color[] beatColors = {Color.red, Color.blue, Color.green};
+
+        private Color restColor = Color.white; // Default to white color
+        private int randomIndex;
+
+        private void Start()
+        {
+            imageComponent = GetComponent<Image>();
+        }
+
+        
+
+        protected override void OnBeat(int biasIndex)
+        {
+            base.OnBeat(biasIndex);
+            StopCoroutine(MoveToColor(beatColors[randomIndex]));
+            randomIndex = Random.Range(0, beatColors.Length);
+            StartCoroutine(MoveToColor(beatColors[randomIndex]));
+        }
+
+        private IEnumerator MoveToColor(Color target)
+        {
+            float timer = 0;
+            Color currentColor = imageComponent.color;
+            while (currentColor != target)
+            {
+                currentColor = Color.Lerp(currentColor, target, timer / timeToBeat);
+                timer += Time.deltaTime;
+                imageComponent.color = currentColor;
+                yield return null;
+            }
+            isBeat = false;
+        }
+    }
+}
