5cee5a037519c303c980d4cddf0dbbf5133ba068
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-01 23:59:55 +0300

    AudioSyncColor is updated
    Audio syncolor is emptied since it was giving too many errors and since I will write it from ground up it is not necessary to have it full of code that will lack necessary dependencies

--- Assets/Rimaethon/_Scripts/MusicSync/AudioSyncColor.cs
+++ Assets/Rimaethon/_Scripts/MusicSync/AudioSyncColor.cs
@@ -1,45 +1,14 @@
 ﻿using System.Collections;
+using Rimaethon._Scripts.MusicSync;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Rimaethon._Scripts.MusicSync
+[RequireComponent(typeof(Image))]
+public class AudioSyncColor : Visualizer
 {
-    [RequireComponent(typeof(Image))]
-    public class AudioSyncColor : AudioSyncer 
-    {
-        private Image imageComponent;
-        private Color[] beatColors = {Color.red, Color.blue, Color.green};
 
-        private Color restColor = Color.white; // Default to white color
-        private int randomIndex;
+    
+ 
 
-        private void Start()
-        {
-            imageComponent = GetComponent<Image>();
-        }
-
-        
-
-        protected override void OnBeat(int biasIndex)
-        {
-            base.OnBeat(biasIndex);
-            StopCoroutine(MoveToColor(beatColors[randomIndex]));
-            randomIndex = Random.Range(0, beatColors.Length);
-            StartCoroutine(MoveToColor(beatColors[randomIndex]));
-        }
-
-        private IEnumerator MoveToColor(Color target)
-        {
-            float timer = 0;
-            Color currentColor = imageComponent.color;
-            while (currentColor != target)
-            {
-                currentColor = Color.Lerp(currentColor, target, timer / timeToBeat);
-                timer += Time.deltaTime;
-                imageComponent.color = currentColor;
-                yield return null;
-            }
-            isBeat = false;
-        }
-    }
+   
 }
