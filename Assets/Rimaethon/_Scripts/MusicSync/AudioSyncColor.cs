b0640de2570d952cd3fc2211eed8a80314f542b5
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-02 23:54:17 +0300

    Update AudioSyncColor.cs
    I'm trying to use peaklevel array to get rgb values to change the color of the sound bar

--- Assets/Rimaethon/_Scripts/MusicSync/AudioSyncColor.cs
+++ Assets/Rimaethon/_Scripts/MusicSync/AudioSyncColor.cs
@@ -1,14 +1,31 @@
-﻿using System.Collections;
-using Rimaethon._Scripts.MusicSync;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Image))]
-public class AudioSyncColor : Visualizer
+namespace Rimaethon._Scripts.MusicSync
 {
+    public interface IAudioSpectrumProvider
+    {
+        float GetPeakLevel(int index);
+    }
 
-    
- 
+    [RequireComponent(typeof(Image))]
+    public class AudioSyncColor : MonoBehaviour
+    {
+        private IAudioSpectrumProvider _audioSpectrumProvider;
+        [SerializeField] private float frequencyToListen;
+        private float normalizedPeakLevel;
 
-   
+        // Dependency Injection through the Awake method
+        private void Awake()
+        {
+            _audioSpectrumProvider = GetComponentInParent<IAudioSpectrumProvider>();
+        }
+
+        private void ChangeColors()
+        {
+            normalizedPeakLevel = _audioSpectrumProvider.GetPeakLevel(0) / 256f;
+            var color = new Color(normalizedPeakLevel, normalizedPeakLevel, normalizedPeakLevel);
+            GetComponent<Image>().color = color;
+        }
+    }
 }
