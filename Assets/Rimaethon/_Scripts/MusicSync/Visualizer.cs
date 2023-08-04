e4f480eb66112258639c91eea17ba154ef5d8a6f
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-04 23:59:42 +0300

    Visualizer updated
    Now it works through interface and uses dependency injection

--- Assets/Rimaethon/_Scripts/MusicSync/Visualizer.cs
+++ Assets/Rimaethon/_Scripts/MusicSync/Visualizer.cs
@@ -6,29 +6,20 @@ namespace Rimaethon._Scripts.MusicSync
     {
         private AudioSpectrum _audioSpectrum;
         private float scaleHolder;
+
         private void Awake()
         {
             _audioSpectrum = GetComponentInParent<AudioSpectrum>();
-            
         }
-        
-        
+
         private void Update()
         {
-
             for (int i = 0; i < _audioSpectrum.PeakLevels.Length; i++)
             {
-                float scale = _audioSpectrum.PeakLevels[i]*100 ;
-                scaleHolder=Mathf.Lerp(transform.GetChild(i).localScale.y, scale, Time.deltaTime * 10f);
-                transform.GetChild(i).localScale =new Vector3(1,scaleHolder,1); 
-                
+                float scale = _audioSpectrum.PeakLevels[i] * 100;
+                scaleHolder = Mathf.Lerp(transform.GetChild(i).localScale.y, scale, Time.deltaTime * 10f);
+                transform.GetChild(i).localScale = new Vector3(1, scaleHolder, 1);
             }
-                
-
         }
-
-
-        
-        
     }
 }
