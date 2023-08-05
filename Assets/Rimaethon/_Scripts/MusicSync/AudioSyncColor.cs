54fb938e81491e280731bd2ed099b9b575a56133
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-05 17:29:00 +0300

    Enum colors added and color changing mechanic is changed
    Now the script works with dividing peak level to mean level and getting an index number with rounding up that number

--- Assets/Rimaethon/_Scripts/MusicSync/AudioSyncColor.cs
+++ Assets/Rimaethon/_Scripts/MusicSync/AudioSyncColor.cs
@@ -3,28 +3,81 @@ using UnityEngine.UI;
 
 namespace Rimaethon._Scripts.MusicSync
 {
-    public partial interface IAudioSpectrumProvider
+    public enum Colors
     {
-        float GetPeakLevel(int index);
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Magenta,
+        Cyan
     }
 
     [RequireComponent(typeof(Image))]
     public class AudioSyncColor : MonoBehaviour
     {
         private IAudioSpectrumProvider _audioSpectrumProvider;
-        [SerializeField] private float frequencyToListen;
-        private float normalizedPeakLevel;
+        private Image _image;
+        [SerializeField] private int frequencyBandIndex = 0; 
+
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+        }
 
         private void Awake()
         {
             _audioSpectrumProvider = GetComponentInParent<IAudioSpectrumProvider>();
         }
 
+        private void Update()
+        {
+            ChangeColors();
+           
+        }
+
         private void ChangeColors()
         {
-            normalizedPeakLevel = _audioSpectrumProvider.GetPeakLevel(0) / 256f;
-            var color = new Color(normalizedPeakLevel, normalizedPeakLevel, normalizedPeakLevel);
-            GetComponent<Image>().color = color;
+            float meanLevel = _audioSpectrumProvider.GetMeanLevel(frequencyBandIndex);
+            float peakLevel = _audioSpectrumProvider.GetPeakLevel(frequencyBandIndex);
+
+            // Avoid division by zero
+            if (peakLevel == 0)
+                return;
+
+            float ratio = meanLevel / peakLevel;
+
+            // Determine the color based on the ratio
+            Colors colorEnum = GetColorByRatio(ratio);
+
+            // Convert the enum to a Unity color
+            Color color = ConvertEnumToColor(colorEnum);
+
+            _image.color = color;
+        }
+
+        private Colors GetColorByRatio(float ratio)
+        {
+            int index = Mathf.FloorToInt(ratio * 6);
+
+            index = Mathf.Clamp(index, 0, 5);
+
+            return (Colors)index;
+        }
+
+        
+        private Color ConvertEnumToColor(Colors colorEnum)
+        {
+            switch (colorEnum)
+            {
+                case Colors.Red: return Color.red;
+                case Colors.Green: return Color.green;
+                case Colors.Blue: return Color.blue;
+                case Colors.Yellow: return Color.yellow;
+                case Colors.Magenta: return Color.magenta;
+                case Colors.Cyan: return Color.cyan;
+                default: return Color.white; 
+            }
         }
     }
 }
