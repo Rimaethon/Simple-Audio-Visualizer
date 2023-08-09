10c400b1ae30a8dea1e43b5f6f63a327906a91bc
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-09 23:45:54 +0300

    Moving assets to appropriate folders

--- Assets/Rimaethon/_Scripts/Core/Interfaces/IAudioDataSource.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Rimaethon._Scripts.Core.Enums
-{
-    public interface IAudioDataSource
-    {
-        void GetSpectrumData(float[] audioData, int channel, FFTWindow windowType);
-    }
-}
--- Assets/Rimaethon/_Scripts/MusicSync/IAudioSpectrumProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace Rimaethon._Scripts.MusicSync
-{
-    public interface IAudioSpectrumProvider
-    {
-        float GetPeakLevel(int index);
-        float GetMeanLevel(int index);
-    }
-}
