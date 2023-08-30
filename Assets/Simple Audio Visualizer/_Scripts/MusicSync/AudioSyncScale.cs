40a77aa00c663c23d18dbaf63303b24be4b66a7f
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-30 23:42:36 +0300

    Add AudioSyncScale Class for Audio Synchronization
    Added a new class 'AudioSyncScale' in the MusicSync package. This class provides functionality to synchronize the scale of objects with the amplitude of the audio, enhancing the visual representation of music. It also includes a method for a smooth scaling transition by using 'SmoothDamp' function, which significantly improves the visual aesthetics while providing a smooth user experience. Tweaked the file renaming for consistency.

---
+++ Assets/Simple Audio Visualizer/_Scripts/MusicSync/AudioSyncScale.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Simple_Audio_Visualizer._Scripts.MusicSync
+{
+    public class AudioSyncScale : AudioPeakListener
+    {
+        private const float TargetScaleDescale = 0.1f;
+        private const float Threshold = 0.06f;
+        private const float LerpTime = 0.1f;
+        [SerializeField] private float scaleMultiplier = 10f;
+
+        private float _targetScale;
+        private float _currentScale;
+        private float _velocity;
+
+        #region Overrided Functions
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _currentScale = transform.localScale.y;
+        }
+
+        protected override void ProcessAmplitude(float amplitude)
+        {
+            _targetScale = amplitude * scaleMultiplier;
+            ScaleObjectsSmoothly();
+        }
+
+
+        #endregion
+    
+        #region Custom Functions
+        
+        
+        private void ScaleObjectsSmoothly()
+        {
+            if (Mathf.Abs(_currentScale - _targetScale) < Threshold)
+            {
+                _targetScale = TargetScaleDescale;
+            }
+
+            _currentScale = Mathf.SmoothDamp(_currentScale, _targetScale, ref _velocity, LerpTime);
+
+            Vector3 newScale = new Vector3(1, _currentScale, 1);
+            transform.localScale = newScale;
+        }
+        
+
+        #endregion
+    }
+}
