441f8eaf2c7da8c6c17dbaf15dbe3792feb44d53
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-30 23:49:11 +0300

    Remove redundant IAudioDataSource interface
    Deleted the IAudioDataSource interface and corresponding meta file from Simple Audio Visualizer. This interface was not being used anywhere in the project, thereby contributing to code bloat. Reduction in code complexity aides in maintainability in the long run.

--- Assets/Simple Audio Visualizer/_Scripts/Core/Interfaces/IAudioDataSource.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Simple_Audio_Visualizer._Scripts.Core.Interfaces
-{
-    public interface IAudioDataSource
-    {
-        void GetSpectrumData(float[] audioData, int channel, FFTWindow windowType);
-    }
-}
