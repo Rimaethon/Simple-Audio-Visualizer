6b82dd47b911ffde6c93884048fa2a3d6cc35c5f
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-30 23:49:58 +0300

    Remove unused IColorChangeable interface
    The IColorChangeable interface and its associated metadata file were removed from the Simple Audio Visualizer project's core script interfaces. This interface was no longer in use, thereby cleaning up the codebase and reducing confusion for future developments.

--- Assets/Simple Audio Visualizer/_Scripts/Core/Interfaces/IColorChangable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Simple_Audio_Visualizer._Scripts.Core.Interfaces
-{
-    public interface IColorChangeable
-    {
-        void ChangeColor(Color newColor);
-    }
-}
